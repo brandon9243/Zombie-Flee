fc3fc48d503bef02b163e099f7446b2afa0bc778
brandon9243 <100967878+brandon9243@users.noreply.github.com>
2024-03-18 14:27:05 -0400

    Organized files and made some changes

---
+++ src/container.js
@@ -0,0 +1,19 @@
+class container {
+    constructor(
+        kills,
+        deaths,
+        upgrades,
+        zombie,
+        fastZombie,
+        tankZombie,
+        crawlingZombie
+    ) {
+        this.kills = kills;
+        this.deaths = deaths;
+        this.upgrades = upgrades;
+        this.zombie = zombie;
+        this.fastZombie = fastZombie;
+        this.tankZombie = tankZombie;
+        this.crawlingZombie = crawlingZombie;
+    }
+}
---
+++ src/main.js
@@ -0,0 +1,17 @@
+let kills = 0;
+let deaths = 0;
+let upgrades = 0;
+let zombie = 1;
+let fastZombie = 0;
+let tankZombie = 0;
+let crawlingZombie = 1;
+
+function killCounter() {}
+
+function deathCounter() {}
+
+function zombies() {}
+
+function upgrades() {}
+
+function updateGame() {}
