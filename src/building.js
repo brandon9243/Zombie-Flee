3dace3eb039057fdb47f4a164de8ef04bcd3c41c
brandon9243 <100967878+brandon9243@users.noreply.github.com>
2024-04-26 13:46:57 -0400

    Added more code and fixed a few things
    Added another constructor and made some changes

--- src/building.js
+++ src/building.js
@@ -1,9 +1,10 @@
-/* class containing our score and cost */
+/* class containing our score and other stuff */
 class Building {
-    constructor(score, zombies, money, time) {
+    constructor(score, zombies, money, time, lives) {
         this.score = score;
         this.zombies = zombies;
         this.money = money;
         this.time = time;
+        this.lives = lives;
     }
 }
--- src/main.js
+++ src/main.js
@@ -2,3 +2,4 @@ let score = 0;
 let zombies = 0;
 let money = 100;
 let time = 0;
+let lives = 5;
