c0e1de3af01c47fef534811d2c8e65d6bc6e2604
brandon9243 <100967878+brandon9243@users.noreply.github.com>
2024-04-26 13:31:55 -0400

    Rearranged files into folders and wrote some code
    Rearranged some files into the folders to organize and wrote some code for main and building js files, I also wrote some code for game.html for the starter screen.

---
+++ src/building.js
@@ -0,0 +1,9 @@
+/* class containing our score and cost */
+class Building {
+    constructor(score, zombies, money, time) {
+        this.score = score;
+        this.zombies = zombies;
+        this.money = money;
+        this.time = time;
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,17 +1,4 @@
-let kills = 0;
-let deaths = 0;
-let upgrades = 0;
-let zombie = 1;
-let fastZombie = 0;
-let tankZombie = 0;
-let crawlingZombie = 1;
-
-function killCounter() {}
-
-function deathCounter() {}
-
-function zombies() {}
-
-function upgrades() {}
-
-function updateGame() {}
+let score = 0;
+let zombies = 0;
+let money = 100;
+let time = 0;
